0ddf2517425bc2e286aab0cd48977a28b939126e
Timo Behrmann <timo.behrmann@gmail.com>
2014-06-29 07:58:13 +0200

    entry types

--- generate.js
+++ generate.js
@@ -16,6 +16,50 @@ db.serialize(function () {
 
 var _ = require('lodash');
 
+
+var prepareIndexEntry = function (method, anchor) {
+    var type = 'Guide';
+
+    if (/^(?:Hapi|plugin).[a-z]/g.test(method)) {
+        type = 'Property';
+    } else if (/^Hapi.[A-Z]/g.test(method)) {
+        type = 'Constructor';
+    }
+
+    if (method.indexOf('(') !== -1) {
+        type = 'Method';
+
+        if (method.indexOf('createServer') === 0) {
+            method = 'Hapi.' + method;
+            type = 'Constructor';
+        } else if (method.indexOf('Pack.compose') === 0) {
+            type = 'Constructor';
+        } else if (method.indexOf('prepareValue') === 0) {
+            method = 'Hapi.state.' + method;
+            type = 'Method';
+        } else if (method.indexOf('message') !== -1) {
+            method = 'Hapi.error.' + method;
+            type = 'Error';
+        } else if (method.indexOf('module.exports') === 0) {
+            type = 'Plugin';
+        }
+    }
+
+    if (method.indexOf('new ') === 0) {
+        type = 'Constructor';
+        method = 'Hapi.' + method.substr(4);
+    } else if (method.indexOf('Interface') !== -1) {
+        type = 'Interface';
+        method = 'Plugin';
+    }
+
+    return {
+        method: method,
+        anchor: anchor,
+        type: type
+    }
+};
+
 var fetchRawMarkdown = function (url) {
     return Q.Promise(function (resolve, reject) {
         request(url, function (err, res, payload) {
@@ -44,59 +88,14 @@ var createSearchIndex = function (markdown) {
         var guidesRegex = /\n- *\[(?:`|)([^`\}\]]*)(?:`|)]\((#[A-Za-z\-]*)\)/g;
 
         while (matches = guidesRegex.exec(markdown)) {
-            var method = matches[1];
-            var anchor = matches[2];
-            var type = 'Guide';
-
-            if (/^Hapi.[a-z]/g.test(method)) {
-                type = 'Property';
-            } else if (/^Hapi.[A-Z]/g.test(method)) {
-                type = 'Constructor';
-            } else if (method.indexOf('(') !== -1) {
-                type = 'Method';
-            }
-
-            stmt.run(method, type, 'reference.html' + anchor);
+            var entry = prepareIndexEntry(matches[1], matches[2]);
+            stmt.run(entry.method, entry.type, 'reference.html' + entry.anchor);
         }
 
         var methodRegex = /\n[\s]*-[\s]*\[`([A-Za-z\.]*.*)`]\((#[A-Za-z\-]*)\)/g;
         while (matches = methodRegex.exec(markdown)) {
-            var method = matches[1];
-            var anchor = matches[2];
-
-            var type = 'Property';
-            if (/^Hapi.[A-Z]/g.test(method)) {
-                var type = 'Constructor';
-            }
-
-            if (method.indexOf('(') !== -1) {
-                type = 'Method';
-
-                if (method.indexOf('createServer') === 0) {
-                    method = 'Hapi.' + method;
-                    type = 'Constructor';
-                } else if (method.indexOf('Pack.compose') === 0) {
-                    type = 'Constructor';
-                } else if (method.indexOf('prepareValue') === 0) {
-                    method = 'Hapi.state.'+method;
-                    type = 'Constructor';
-                } else if (method.indexOf('message') !== -1) {
-                    method = 'Hapi.error.' + method;
-                    type = 'Error';
-                } else if (method.indexOf('module.exports') === 0) {
-                    type = 'Plugin';
-                }
-            }
-
-            if (method.indexOf('new ') === 0) {
-                type = 'Constructor';
-                method = 'Hapi.' + method.substr(4);
-            } else if (method.indexOf('Interface') !== -1) {
-                type = 'Interface';
-                method = 'Plugin';
-            }
-
-            stmt.run(method, type, 'reference.html' + anchor);
+            var entry = prepareIndexEntry(matches[1], matches[2]);
+            stmt.run(entry.method, entry.type, 'reference.html' + entry.anchor);
         }
 
         stmt.finalize();
@@ -144,13 +143,13 @@ var replaceUserContent = function (text) {
     });
 };
 
-var addDashAnchors = function(text) {
-    return Q.promise(function(resolve) {
-        db.each('SELECT name, type, path FROM searchIndex', function(err, row) {
-            var dashAnchor = '<a name="//apple_ref/cpp/'+row.type+'/'+encodeURIComponent(row.name)+'" class="dashAnchor"></a>';
+var addDashAnchors = function (text) {
+    return Q.promise(function (resolve) {
+        db.each('SELECT name, type, path FROM searchIndex', function (err, row) {
+            var dashAnchor = '<a name="//apple_ref/cpp/' + row.type + '/' + encodeURIComponent(row.name) + '" class="dashAnchor"></a>';
             var searchTerm = '<a name="' + row.path.split('#')[1] + '"';
-            text = text.replace(new RegExp(searchTerm, 'g'), dashAnchor+searchTerm);
-        }, function() {
+            text = text.replace(new RegExp(searchTerm, 'g'), dashAnchor + searchTerm);
+        }, function () {
             return resolve(text);
         });
     });
@@ -187,6 +186,6 @@ fetchRawMarkdown(referenceUrl)
     .then(writeFile)
     .then(function (markdown) {
         console.log('Generation completed!');
-    }).catch(function(e) {
+    }).catch(function (e) {
         console.log(e);
     });
